ecb0b329cff540cd29e2213f8d7cbdc7cf56d61c
GyewanKim <87048326+larpp@users.noreply.github.com>
2024-04-23 14:44:14 +0900

    Update data.py

--- data.py
+++ data.py
@@ -1,13 +1,18 @@
 from dataset import CocoDetection
 from torch.utils.data import DataLoader
 from collate import collate_fn
-from transformers import DetrImageProcessor
 from argparse_utils import parse_args
 
 
 def make_dataset(mode_directory, train=False):
     args = parse_args()
-    image_processor = DetrImageProcessor.from_pretrained(args.checkpoint)
+
+    if args.checkpoint == 'facebook/detr-resnet-50':
+        from transformers import DetrImageProcessor
+        image_processor = DetrImageProcessor.from_pretrained(args.checkpoint)
+    else:
+        from transformers import AutoImageProcessor
+        image_processor = AutoImageProcessor.from_pretrained(args.checkpoint)
 
     # Data Loader
     DATASET = CocoDetection(
@@ -19,7 +24,8 @@ def make_dataset(mode_directory, train=False):
 
 
 def make_dataloader(mode_directory, batch_size, train=False, shuffle=False):
-    DATASET = make_dataset(mode_directory, train)
-    DATALOADER = DataLoader(dataset=DATASET, collate_fn=collate_fn, batch_size=batch_size, shuffle=shuffle, num_workers=4)
+    args = parse_args()
+    DATASET = make_dataset(mode_directory, train=train)
+    DATALOADER = DataLoader(dataset=DATASET, collate_fn=collate_fn, batch_size=batch_size, shuffle=shuffle, num_workers=args.num_workers)
 
     return DATALOADER
